2cafd83d0456595039a5ca000f103d33ce09349f
tekuto <g.tekuto@gmail.com>
2026-05-06 04:44:36 +0900

    tktiffuserswrapper.cli.main.main()の処理を修正

--- tktiffuserswrapper/cli/main.py
+++ tktiffuserswrapper/cli/main.py
@@ -1,14 +1,24 @@
+from tktiffuserswrapper import config
+import importlib
 import sys
 
 def main(
 ):
     if len( sys.argv ) < 2:
-        print( "使い方: 設定モジュール" )
+        print( "使い方: 設定モジュールパス" )
         return -1
 
-    SETUP_MODULE = sys.argv[ 1 ]
+    SETUP_MODULE_PATH = sys.argv[ 1 ]
+
+    SETUP_MODULE = importlib.import_module( SETUP_MODULE_PATH )
 
     while True:
         input( "ENTERで生成を開始します" )
+
+        config.reset()
+
+        importlib.reload( SETUP_MODULE )
+
+        #TODO
 
     return 0
