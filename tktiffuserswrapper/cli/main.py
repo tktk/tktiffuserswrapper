dd288652f997a92b633d93e3198afaf7fb61a425
tekuto <g.tekuto@gmail.com>
2026-05-06 04:02:22 +0900

    tktiffuserswrapper.cli.main.main()にコマンドライン引数の数チェックを追加

--- tktiffuserswrapper/cli/main.py
+++ tktiffuserswrapper/cli/main.py
@@ -1,4 +1,14 @@
+import sys
+
 def main(
 ):
-    #TODO
+    if len( sys.argv ) < 2:
+        print( "使い方: 設定モジュール" )
+        return -1
+
+    SETUP_MODULE = sys.argv[ 1 ]
+
+    while True:
+        input( "ENTERで生成を開始します" )
+
     return 0
