fcedf5dd339263f1556df64bad554fe7c2555911
tekuto <g.tekuto@gmail.com>
2026-05-07 02:42:01 +0900

    tktiffuserswrapper.tokenizer.promptIsTooLong()の処理を追加

--- tktiffuserswrapper/tokenizer.py
+++ tktiffuserswrapper/tokenizer.py
@@ -2,5 +2,18 @@ def promptIsTooLong(
     _tokenizer,
     _PROMPT,
 ):
-    #TODO
+    TOKEN_IDS = _tokenizer.encode(
+        _PROMPT,
+        add_special_tokens = True,
+    )
+    TOKEN_IDS_LENGTH = len( TOKEN_IDS )
+    MODEL_MAX_LENGTH = _tokenizer.model_max_length
+
+    if TOKEN_IDS_LENGTH > MODEL_MAX_LENGTH:
+        TOKENS = _tokenizer.tokenize( _PROMPT )
+        TOKENS_LENGTH = len( TOKENS )
+        MODEL_MAX_LENGTH_EFFECTIVE = MODEL_MAX_LENGTH - ( TOKEN_IDS_LENGTH - TOKENS_LENGTH )
+
+        return TOKENS_LENGTH, MODEL_MAX_LENGTH_EFFECTIVE, TOKENS[ MODEL_MAX_LENGTH_EFFECTIVE : ]
+
     return None, None, None
