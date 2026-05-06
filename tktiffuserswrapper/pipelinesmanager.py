8bbe4a29fb7e253748828d93b80d329a934a3c83
tekuto <g.tekuto@gmail.com>
2026-05-06 19:12:09 +0900

    tktiffuserswrapper.pipelinesmanager.PipelinesManager#__init__()の処理を追加

--- tktiffuserswrapper/pipelinesmanager.py
+++ tktiffuserswrapper/pipelinesmanager.py
@@ -1,6 +1,6 @@
 class PipelinesManager:
     def __init__( _self ):
-        _self._beforeGeneratePipelines = 10 # TODO
+        _self._beforeGeneratePipelines = None
         _self._pipelineForT2I = None
         _self._pipelineForI2I = None
         _self._pipelineForInpaint = None
