57165224be3a883762c77852a5aa7a5c775a2def
tekuto <g.tekuto@gmail.com>
2026-05-06 20:56:56 +0900

    tktiffuserswrapper.pipelinesmanager.PipelineManager#generatePipelines()のテストを追加

--- tests/test_pipelinesmanager_generatepipelines.py
+++ tests/test_pipelinesmanager_generatepipelines.py
@@ -6,6 +6,10 @@ _PIPELINE_FOR_TEXT2IMAGE = 10
 _PIPELINE_FOR_IMAGE2IMAGE = 20
 _PIPELINE_FOR_INPAINT = 30
 
+_BEFORE_PIPELINE_FOR_TEXT2IMAGE = 40
+_BEFORE_PIPELINE_FOR_IMAGE2IMAGE = 50
+_BEFORE_PIPELINE_FOR_INPAINT = 60
+
 class TestPipelinesManagerGeneratePipelines( unittest.TestCase ):
     def test( _self ):
         pipelinesManager = PipelinesManager()
@@ -17,7 +21,20 @@ class TestPipelinesManagerGeneratePipelines( unittest.TestCase ):
         _self.assertEqual( _PIPELINE_FOR_IMAGE2IMAGE, pipelinesManager._pipelineForImage2Image )
         _self.assertEqual( _PIPELINE_FOR_INPAINT, pipelinesManager._pipelineForInpaint )
 
-    # TODO testAlreadyGenerated
+    def testAlreadyGenerated( _self ):
+        pipelinesManager = PipelinesManager()
+
+        pipelinesManager._beforeGeneratePipelines = _generatePipelines
+        pipelinesManager._pipelineForText2Image = _BEFORE_PIPELINE_FOR_TEXT2IMAGE
+        pipelinesManager._pipelineForImage2Image = _BEFORE_PIPELINE_FOR_IMAGE2IMAGE
+        pipelinesManager._pipelineForInpaint = _BEFORE_PIPELINE_FOR_INPAINT
+
+        pipelinesManager.generatePipelines( _generatePipelines )
+
+        _self.assertEqual( _BEFORE_PIPELINE_FOR_TEXT2IMAGE, pipelinesManager._pipelineForText2Image )
+        _self.assertEqual( _BEFORE_PIPELINE_FOR_IMAGE2IMAGE, pipelinesManager._pipelineForImage2Image )
+        _self.assertEqual( _BEFORE_PIPELINE_FOR_INPAINT, pipelinesManager._pipelineForInpaint )
+
     # TODO testChangedGeneratePipelines
 
 def _generatePipelines(
