7256f21222dbd84bc619116ef46d04c2bd9d11ee
tekuto <g.tekuto@gmail.com>
2026-05-06 23:51:00 +0900

    tktiffuserswrapper.reset()の処理を修正

--- tktiffuserswrapper/__init__.py
+++ tktiffuserswrapper/__init__.py
@@ -29,6 +29,7 @@ def reset(
     global GENERATE_PIPELINE_ARGS
     global GET_TOKENIZERS
     global GET_PROMPT_KEYS
+    global MANUAL_SEED
     global PNG_INFO_EXTRAS
     global OUTPUT_PATH_PREFIX
     global POST_PROCESS
@@ -38,6 +39,7 @@ def reset(
     GENERATE_PIPELINE_ARGS = None
     GET_TOKENIZERS = _getTokenizers
     GET_PROMPT_KEYS = _getPromptKeys
+    MANUAL_SEED = None
     PNG_INFO_EXTRAS = {}
     OUTPUT_PATH_PREFIX = "output"
     POST_PROCESS = _postProcess
