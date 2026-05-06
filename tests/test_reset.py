3ee57d6d10506fcadb7ccc4e614aedb720851aa4
tekuto <g.tekuto@gmail.com>
2026-05-06 23:33:31 +0900

    tktiffuserswrapper.reset()のテストを修正

--- tests/test_reset.py
+++ tests/test_reset.py
@@ -4,18 +4,38 @@ import tktiffuserswrapper as tw
 import inspect
 
 class TestReset( unittest.TestCase ):
+    def _assertCallable(
+        _self,
+        _CALLABLE,
+        _EXPECTED_PARAMTERS_COUNT,
+    ):
+        _self.assertTrue( callable( _CALLABLE ) )
+        SIGNATURE = inspect.signature( _CALLABLE )
+        _self.assertEqual( _EXPECTED_PARAMTERS_COUNT, len( SIGNATURE.parameters ) )
+
     def test( _self ):
         tw.reset()
 
         _self.assertIsNone( tw.GENERATE_PIPELINES )
         _self.assertEqual( 1, tw.GENERATION_COUNT )
         _self.assertIsNone( tw.GENERATE_PIPELINE_ARGS )
+
+        _self._assertCallable(
+            tw.GET_TOKENIZERS,
+            1,
+        )
+        _self._assertCallable(
+            tw.GET_PROMPT_KEYS,
+            0,
+        )
+
         _self.assertDictEqual( {}, tw.PNG_INFO_EXTRAS )
         _self.assertEqual( "output", tw.OUTPUT_PATH_PREFIX )
 
-        _self.assertTrue( callable( tw.POST_PROCESS ) )
-        SIGNATURE = inspect.signature( tw.POST_PROCESS )
-        _self.assertEqual( 1, len( SIGNATURE.parameters ) )
+        _self._assertCallable(
+            tw.POST_PROCESS,
+            1,
+        )
 
 if __name__ == '__main__':
     unittest.main()
