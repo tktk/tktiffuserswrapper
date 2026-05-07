1dbe6402a9034d4f7c42dc324f00e6d55be78cdc
tekuto <g.tekuto@gmail.com>
2026-05-08 03:56:22 +0900

    tktiffuserswrapper.pnginfo.generatePngInfo()の処理を追加

--- tktiffuserswrapper/pnginfo.py
+++ tktiffuserswrapper/pnginfo.py
@@ -1,5 +1,33 @@
+from PIL.PngImagePlugin import PngInfo
+
+def _addTextToPngInfo(
+    _pngInfo,
+    _KEY,
+    _VALUE,
+):
+    _pngInfo.add_text(
+        f"tktiffuserswrapper.{_KEY}",
+        str( _VALUE ),
+    )
+
 def generatePngInfo(
     _PIPELINE_ARGS,
     _EXTRAS,
 ):
-    pass
+    pngInfo = PngInfo()
+
+    for ( KEY, VALUE ) in _PIPELINE_ARGS.items():
+        _addTextToPngInfo(
+            pngInfo,
+            f"PIPELINE_ARGS.{KEY}",
+            VALUE,
+        )
+
+    for ( KEY, VALUE ) in _EXTRAS.items():
+        _addTextToPngInfo(
+            pngInfo,
+            f"EXTRAS.{KEY}",
+            VALUE,
+        )
+
+    return pngInfo
